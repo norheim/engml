c4066348a7a737a826090536be7e91b093ca04c6
norheim <norheim@mit.edu>
2018-05-29 13:43:34 +0200

    Modularized depexpressions and returns propagation chain

--- depexpressions.py
+++ depexpressions.py
@@ -8,40 +8,30 @@ class Expression():
     def __init__(self):
         self.G = DynamicGraph(nx.DiGraph())
 
+    def propagate(self, exprname):
+        return self.G.propagate(exprname)
+
     # Adding and updating currently are the same thing
     def add(self, name, dependencies=[]):
         if len(dependencies) == 0 and name not in self.G.G.nodes():
             self.G.add_node(name)
+            return self.propagate(name)
         else:
             if name in self.G.G.nodes():
                 existing_deps = list(self.G.G.predecessors(name))
             else:
                 existing_deps = []
-            print(existing_deps)
-            #Remove existing dependencies
+            # print(existing_deps)
+            # Remove existing dependencies
             for dep in existing_deps:
                 if dep not in dependencies:
                     self.G.remove_edge(dep, name)
-            #Add new dependencies
+            # Add new dependencies
             for dep in dependencies:
                 if dep not in existing_deps:
                     self.G.add_edge(dep, name)
-
-e = Expression()
-e.add(5)
-e.add(7)
-e.add(3)
-print_topo(e.G.topo)
-e.add(11, [5, 7])
-e.add(8, [7, 3])
-e.add(2, [11])
-e.add(9, [11])
-e.add(10, [3])
-e.add(7, [10])
-e.add(7, [])
-e.G.propagate(10)
-# technically an update
-
+            # Finish by propagating changes
+            return self.propagate(name)
 class System():
     def __init__(self):
         pass
---
+++ depexpressionstest.py
@@ -0,0 +1,26 @@
+from depexpressions import Expression
+
+# Example code for playing with expressions
+e = Expression()
+
+# Create independent expressions
+e.add(5)
+e.add(7)
+e.add(3)
+
+# Dependent expression definitions
+e.add(11, [5, 7])
+e.add(8, [7, 3])
+e.add(2, [11])
+e.add(9, [11])
+e.add(10, [3])
+
+# Updating dependency definitions
+e.add(7, [10])
+
+# Propagating independent expressions changes
+e.propagate(3)
+
+# Redifinition again
+e.add(7, [])
+e.propagate(3)
